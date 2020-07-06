10022b78baa5ff867468c454e9ea8c2b6697cc20
Mosa M.Sebdani <msebdani@gmail.com>
2020-07-06 14:25:58 +0200

    set user

--- frontend/src/components/CountriesList.js
+++ frontend/src/components/CountriesList.js
@@ -3,12 +3,12 @@ import CountryCard from "./CountryCard";
 import Button from "../elements/Button"
 import Title from "../elements/Title"
 
-import Div from "../elements/Div"
 
 export default function CountriesList({ rates = [], countries, clearList, remove }) {
   if (!countries) return (<small>List is empty, add some countries to compare exchange rates!</small>)
 
   if (countries !== []) return (
+
     <div style={{ margin:'12px', padding: "12px", borderTop: "1px solid gray", borderBottom: "1px solid gray"}}>
       <Title title="Saved countries:" />
 
@@ -16,7 +16,8 @@ export default function CountriesList({ rates = [], countries, clearList, remove
 
         <CountryCard key={country.name} country={country} rates={rates} fn={remove} />
       ))}
-      
+
+
       <Button
         content="clear list"
         fn={clearList}
